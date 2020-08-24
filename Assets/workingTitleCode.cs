1940c849995bf7d5320e233d3cd795d1370c2f39
siky112 <unfixedacorn@gmail.com>
2020-08-23 17:42:47 -0700

    Aesthetic changes and code optimizations

---
+++ Assets/workingTitleCode.cs
@@ -0,0 +1,147 @@
+﻿//Hey, welcome to my code. I'm sorry for breaking every style rule in existence.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using KModkit;
+
+public class workingTitleCode : MonoBehaviour 
+{
+	public KMBombInfo edgework;
+	public KMAudio sound;
+	
+	public KMSelectable button1;
+	public KMSelectable button2;
+	public KMSelectable button3;
+	public TextMesh screenText;
+	
+	public string[] aText;
+	public string[] bText;
+	public string[] cText;
+	public int buttonToPress;
+	public int textUsed;
+	
+	static int moduleIdCounter = 1;
+	int moduleId;
+	private bool textPicked = false;
+	private bool moduleSolved; 
+	
+	void Awake ()
+	{
+		moduleId = moduleIdCounter++;
+		button1.OnInteract += delegate () { PressButton1(); return false; };
+		button2.OnInteract += delegate () { PressButton2(); return false; };
+		button3.OnInteract += delegate () { PressButton3(); return false; };
+	}
+	
+	void Start () 
+	{
+		if (textPicked == false)
+		{
+			PickText();
+			textPicked = true;
+		}
+	}
+	
+	void PickText()
+	{
+		buttonToPress = UnityEngine.Random.Range(0,3);
+		textUsed = UnityEngine.Random.Range(0,25);
+		switch (buttonToPress)
+		{
+			case 0:
+				screenText.text = aText[textUsed];
+				Debug.LogFormat("[Working Title #{0}] The screen says {1}.", moduleId, aText[textUsed]);
+				Debug.LogFormat("[Working Title #{0}] You have to push Button A.", moduleId);
+				break;
+		
+			case 1:
+				screenText.text = bText[textUsed];
+				Debug.LogFormat("[Working Title #{0}] The screen says {1}.", moduleId, bText[textUsed]);
+				Debug.LogFormat("[Working Title #{0}] You have to push Button B.", moduleId);
+				break;
+		
+			case 2:
+				screenText.text = cText[textUsed];
+				Debug.LogFormat("[Working Title #{0}] The screen says {1}.", moduleId, cText[textUsed]);
+				Debug.LogFormat("[Working Title #{0}] You have to push Button C.", moduleId);
+				break;
+				
+			default:
+				Debug.LogFormat("[Working Title #{0}] Uh oh, something's wrong! Panic!", moduleId);
+				break;
+		}
+	}
+	
+	void PressButton1 ()
+	{
+		Debug.LogFormat("[Working Title #{0}] You pushed Button A.", moduleId);
+		button1.AddInteractionPunch();
+		GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+		if (moduleSolved == false)
+		{
+			if (buttonToPress == 0)
+			{
+				moduleSolved = true;
+				GetComponent<KMBombModule>().HandlePass();
+				screenText.text = "solve text";
+				Debug.LogFormat("[Working Title #{0}] Correct, module solved.", moduleId);
+				
+			}
+			else
+			{
+				Debug.LogFormat("[Working Title #{0}] Wrong, module striked.", moduleId);
+				GetComponent<KMBombModule>().HandleStrike();
+			}
+		}
+	}
+	
+	void PressButton2 ()
+	{
+		Debug.LogFormat("[Working Title #{0}] You pushed Button B.", moduleId);
+		button2.AddInteractionPunch();
+		GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+		if (moduleSolved == false)
+		{
+			if (buttonToPress == 1)
+			{
+				moduleSolved = true;
+				GetComponent<KMBombModule>().HandlePass();
+				screenText.text = "solve text";
+				Debug.LogFormat("[Working Title #{0}] Correct, module solved.", moduleId);
+				
+			}
+			else
+			{
+				Debug.LogFormat("[Working Title #{0}] Wrong, module striked.", moduleId);
+				GetComponent<KMBombModule>().HandleStrike();
+			}
+		}
+	}
+	
+	void PressButton3 ()
+	{
+		Debug.LogFormat("[Working Title #{0}] You pushed Button C.", moduleId);
+		button3.AddInteractionPunch();
+		GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+		if (moduleSolved == false)
+		{
+			if (buttonToPress == 2)
+			{
+				moduleSolved = true;
+				GetComponent<KMBombModule>().HandlePass();
+				screenText.text = "solve text";
+				Debug.LogFormat("[Working Title #{0}] Correct, module solved.", moduleId);
+				
+			}
+			else
+			{
+				Debug.LogFormat("[Working Title #{0}] Wrong, module striked.", moduleId);
+				GetComponent<KMBombModule>().HandleStrike();
+			}
+		}
+	}
+	
+}
